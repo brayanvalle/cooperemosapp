f037b716f7ff80e4fb9611e84a833ca168eab713
Brayan Valle <brayanvallejaramillo@gmail.com>
2020-11-22 18:48:50 -0500

    posts

--- app/controllers/PostController.php
+++ app/controllers/PostController.php
@@ -5,11 +5,9 @@ use Phalcon\Paginator\Adapter\Model as Paginator;
 
 class PostController extends ControllerBase
 {
-    function initialize() {
+    public function onConstruct(){
         $this->postEntryService = new PostEntryService();
     }
-
-
 
     public function indexAction()
     {
@@ -26,6 +24,29 @@ class PostController extends ControllerBase
         $user = $this->getSession();
     }
 
+    public function viewAction($postId){
+        $postEntry = PostEntry::findFirst($postId);
+
+        $this->view->post = $postEntry;
+    }
+
+    public function doShareAction($postId){
+        try{
+            $user = $this->getSession();
+
+            if($postId == "")
+                return;
+
+            $postUserInteraction = new PostUserInteraction();
+            $postUserInteraction->PostEntryId = $postId;
+            $postUserInteraction->IdentityUserId = $user['UserId'];
+            $this->postEntryService->doPostUserInteraction($postUserInteraction, 'SHARE');
+            return $this->Ok('Ok');
+        }
+        catch(Error $e){
+            return $this->Error($e->getMessage());
+        }        
+    }
     public function doLikeAction($postId)
     {
         try{
@@ -34,21 +55,32 @@ class PostController extends ControllerBase
             if($postId == "")
                 return;
 
-            $postUserInteraction = new PostUserInteraction();
-
-            $postUserInteraction->PostEntryId = $postId;
-            $postUserInteraction->IdentityUserId = $user['UserId'];
-
-            $this->postEntryService->doPostUserInteraction($postUserInteraction, 'LIKE');
-            return $this->Ok("Created Ok");
+            // find if like action exists
+            $puiExists = PostUserInteraction::findFirst([
+                'conditions' => 'IdentityUserId = ' . $user['UserId'] . ' AND PostEntryId = ' . $postId 
+            ]);          
+            
+            if(!$puiExists){
+                $postUserInteraction = new PostUserInteraction();
+                $postUserInteraction->PostEntryId = $postId;
+                $postUserInteraction->IdentityUserId = $user['UserId'];
+                $this->postEntryService->doPostUserInteraction($postUserInteraction, 'LIKE');
+                return $this->Ok([
+                    'action' => 'LIKE'
+                ]);
+            }else{
+                $this->postEntryService->doPostUserInteraction($puiExists, 'DISLIKE');
+                return $this->Ok([
+                    'action' => 'DISLIKE'
+                ]); 
+            }
         }
         catch(PDOException $e){
             return $this->Error($e->getMessage());
         }
         catch(Error $e){
             return $this->Error($e->getMessage());
-        }
-        
+        }        
     }
 
 }
--- app/services/PostEntryService.php
+++ app/services/PostEntryService.php
@@ -20,12 +20,24 @@ class PostEntryService{
     }
 
     public function doPostUserInteraction(PostUserInteraction $postUserInteraction, $type){
+        if($type === 'DISLIKE'){
+            $postUserInteraction->PostEntry->LikesCount = $postUserInteraction->PostEntry->LikesCount - 1;
+            $postUserInteraction->save();
+            $postUserInteraction->delete();
+            return;
+        }
+
         switch($type){
             case 'LIKE' :
                 $postUserInteraction->PostUserInteractionTypeId = 1;
                 $postUserInteraction->PostEntry->LikesCount = $postUserInteraction->PostEntry->LikesCount + 1;
                 break;
-            case 'SHARE': $$postUserInteraction->PostUserInteractionTypeId = 2;
+            case 'SHARE': 
+                $postUserInteraction->PostUserInteractionTypeId = 2;
+                $postUserInteraction->PostEntry->ShareCount = $postUserInteraction->PostEntry->ShareCount + 1;
+
+                $postUserInteraction->PostEntry->save();            
+                break;
         }
 
         $postUserInteraction->Date = Utils::Date()->getDate();
