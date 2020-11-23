2f23038dd1a2295887e419bbbfbf0bf2edd78d57
saravj355 <saravj971@gmail.com>
2020-11-22 22:01:59 -0500

    plugins view

---
+++ app/controllers/PluginsController.php
@@ -0,0 +1,12 @@
+<?php
+
+use Phalcon\Mvc\Model\Criteria;
+use Phalcon\Paginator\Adapter\Model as Paginator;
+
+
+class PluginsController extends ControllerBase
+{
+    public function indexAction(){
+        
+    }
+}
